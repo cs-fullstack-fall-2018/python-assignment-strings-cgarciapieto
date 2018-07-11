f6d0d6a326bd8c351deae0169eb501acc651c6b7
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-11 15:36:04 -0500

    Checkin of Excercises

---
+++ exampleGit1.py
@@ -0,0 +1,23 @@
+import os
+os.system("clear")
+
+bandMate = "ringo"
+#1. length of name
+print("the name is this long", len(bandMate))
+print(bandMate.upper())
+print(bandMate.lower())
+print(bandMate.upper()[2])
+print(bandMate[2:])
+print(bandMate[3:])
+
+
+name = input("input a name")
+print(name)
+print("the name is", len(name),"amount of letters long")
+
+print(name.upper())
+
+
+
+
+
